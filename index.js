54d6f82d45dd25690fe3914e36d97b7221aa05d9
PN-Barnes <paulieb99@gmail.com>
2021-07-25 14:12:02 -0700

    started writing the inquirer and fucntions to work throug command application

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+
+const options = [
+    {
+        type: 'list',
+        message: 'Choose what you would like to do!',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+        name: 'options'
+    }
+]
