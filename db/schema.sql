73474d3412eda7465994ce7f1375c303a5f854b4
PN-Barnes <paulieb99@gmail.com>
2021-07-26 21:55:55 -0700

    trying to fix database before to run the functions without exiting the program

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,25 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
+    id INT NOT NULL,
+    title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT ,
-    first_name VARCHAR(30) NOT NULL,
-    last_name DECIMAL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
-
-)
+    id INT NOT NULL ,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL
+    
+);
