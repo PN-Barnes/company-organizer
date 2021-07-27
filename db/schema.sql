970344dba34c5495b253f7b6209ad06bfdd32c7b
PN-Barnes <paulieb99@gmail.com>
2021-07-27 14:35:11 -0700

    attempting fix to schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,28 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    dept_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (dept_id)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    role_id INT NOT NULL AUTO_INCREMENT,
+    role_id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30),
-    salary INT,
-    department INT,
-    PRIMARY KEY (role_id)
-    FOREIGN KEY (department)
-    REFERENCES department(dept_id)
+    salary DECIMAL, 
+    dept INT,
+    FOREIGN KEY (dept)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    employee_id INT AUTO_INCREMENT,
+    employee_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    roleRefId INT,
+    roleRefId INT
     PRIMARY KEY employee_id
     FOREIGN KEY (roleRefId)
     REFERENCES roles(role_id)
+    ON DELETE SET NULL
 );
