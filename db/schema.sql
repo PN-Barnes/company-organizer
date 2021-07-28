d7f517dc2f244aba7e599f817e66c4e52581f585
PN-Barnes <paulieb99@gmail.com>
2021-07-27 18:07:14 -0700

    modified schema.sql file for better syntax

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL UNSIGNED NOT NULL, 
+    salary DECIMAL(8,2) NOT NULL, 
     deptId INT,
     FOREIGN KEY (deptId)
     REFERENCES department(id)
